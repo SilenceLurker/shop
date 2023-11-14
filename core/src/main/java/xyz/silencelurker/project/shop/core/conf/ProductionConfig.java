ddb18b48287251c717bd708a29a8afc3493690bf
Silence_Lurker <slvccans@gmail.com>
2023-11-15 01:23:17 +0800

    update

--- core/src/main/java/xyz/silencelurker/project/shop/core/conf/ProductionConfig.java
+++ core/src/main/java/xyz/silencelurker/project/shop/core/conf/ProductionConfig.java
@@ -5,6 +5,7 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import xyz.silencelurker.project.shop.productionapi.service.IBrandService;
 import xyz.silencelurker.project.shop.productionapi.service.IProductionService;
 
 /**
@@ -16,8 +17,17 @@ public class ProductionConfig {
     @DubboReference(version = "0.0.1-SNAPSHOT")
     IProductionService productionService;
 
+    @DubboReference(version = "0.0.1-SNAPSHOT")
+    IBrandService brandService;
+
     @Bean
-    public IProductionService productionService() {
+    IProductionService productionService() {
         return productionService;
     }
+
+    @Bean
+    IBrandService brandService() {
+        return brandService;
+    }
+
 }
---
+++ production-service/src/main/java/xyz/silencelurker/project/shop/productionservice/service/IBrandServiceImpl.java
@@ -0,0 +1,38 @@
+package xyz.silencelurker.project.shop.productionservice.service;
+
+import java.util.List;
+
+import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import jakarta.annotation.Resource;
+import xyz.silencelurker.project.shop.productionapi.entity.Brand;
+import xyz.silencelurker.project.shop.productionapi.repository.BrandRepository;
+import xyz.silencelurker.project.shop.productionapi.service.IBrandService;
+
+/**
+ * @author Silence_Lurker
+ */
+@DubboService(version = "0.0.1-SNAPSHOT")
+public class IBrandServiceImpl implements IBrandService {
+
+    @Resource
+    private BrandRepository brandRepository;
+
+    @Override
+    public List<Brand> getAllBrand() {
+        return brandRepository.findAll();
+    }
+
+    @Override
+    public List<Brand> getAllBrandByName(String name) {
+        return brandRepository.findAllByBrandNameContaining(name);
+    }
+
+    @Override
+    public Page<Brand> getAllBrandByName(String name, Pageable pageable) {
+        return brandRepository.findAllByBrandNameContaining(name, pageable);
+    }
+
+}
