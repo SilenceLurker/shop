dabf033f22b0bc401ad584473a64ea0b1691f6d7
Silence_Lurker <slvccans@gmail.com>
2024-01-13 22:04:54 +0800

    config

---
+++ easy-shop/src/main/java/xyz/silencelurker/project/shop/easyshop/config/WebConfig.java
@@ -0,0 +1,20 @@
+package xyz.silencelurker.project.shop.easyshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author sl-gpt
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .allowCredentials(true);
+    }
+}
