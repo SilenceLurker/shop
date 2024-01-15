e92ab962a8d80e98b23d04ac16a8b393bb365859
Silence_Lurker <slvccans@gmail.com>
2024-01-15 20:48:52 +0800

    fix

--- easy-shop/src/main/java/xyz/silencelurker/project/shop/easyshop/entity/Color.java
+++ easy-shop/src/main/java/xyz/silencelurker/project/shop/easyshop/entity/Color.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
@@ -23,7 +21,6 @@ import lombok.Data;
 public class Color {
     @Id
     @Column(name = "color_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String image;
